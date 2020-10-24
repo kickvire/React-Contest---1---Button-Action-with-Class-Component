1d8a4d341f1981d6cfa58e5d47bb16a3058d1cc3
Varun kumar <45607763+kickvire@users.noreply.github.com>
2020-10-24 09:56:38 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,31 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.showText = this.showText.bind(this);
+  }
+  showText() {
+    this.setState({ isClicked: true });
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  render() {
+    return (
+      <div id="main">
+        <button id="btn" onClick={this.showText}>
+          Click
+        </button>
+        {this.state.isClicked && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
